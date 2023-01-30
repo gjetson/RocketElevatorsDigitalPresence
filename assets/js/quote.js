e096dc42033aac8b21942cef78b0e52d517a0c50
Del Barnwell <dbarnwel@google.com>
2023-01-30 16:16:31 -0500

    The first 3 radio buttons are connected and working properly.

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,1 +1,24 @@
-//js foo goes here
+
+document.getElementById('res_btn').addEventListener('click', () => {
+    console.log('hello world')
+    document.getElementById('num_apts_in_grp').style.display = 'block'
+    document.getElementById('num_flrs_in_grp').style.display = 'block'
+    document.getElementById('num_vtrs_in_grp').style.display = 'none'
+    document.getElementById('max_occ_in_grp').style.display = 'none'
+})
+
+document.getElementById('comm_btn').addEventListener('click', () => {
+    console.log('hello world')
+    document.getElementById('num_apts_in_grp').style.display = 'none'
+    document.getElementById('num_flrs_in_grp').style.display = 'block'
+    document.getElementById('num_vtrs_in_grp').style.display = 'none'
+    document.getElementById('max_occ_in_grp').style.display = 'block'
+})
+
+document.getElementById('ind_btn').addEventListener('click', () => {
+    console.log('hello world')
+    document.getElementById('num_apts_in_grp').style.display = 'none'
+    document.getElementById('num_flrs_in_grp').style.display = 'none'
+    document.getElementById('num_vtrs_in_grp').style.display = 'block'
+    document.getElementById('max_occ_in_grp').style.display = 'none'
+})
